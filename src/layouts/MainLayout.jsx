862ac8325b62d89d50ac02957547195966d03dfb
Maxim <MaximK38136@yandex.ru>
2024-10-09 18:31:14 +0400

    main page adaptive ready

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Header from '../components/Header'
+import HeaderMobile from '../components/HeaderMobile'
 import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
+  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+  React.useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <Header />
+      { screenWidth >= 1400 && <Header />}
+      { screenWidth < 1400 && <HeaderMobile />}
       <Outlet />
       <Footer />
     </>

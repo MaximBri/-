6b2b3f17acff788131b5197275f942c58ba2d066
polopo927 <polopo927@mail.ru>
2024-10-12 13:36:12 +0300

    fix null

--- src/components/shop/Forms.jsx
+++ src/components/shop/Forms.jsx
@@ -3,7 +3,7 @@ import '../../css/shop/forms.css'
 import { useState } from 'react'
 
 export const Forms = ({ handleClick, isInCart }) => {
-  const [isActiveSize, setIsActiveSize] = useState(null)
+  const [isActiveSize, setIsActiveSize] = useState(0)
 
   const handleClickSize = (itemId, index) => {
     setIsActiveSize(prevSize => ({
